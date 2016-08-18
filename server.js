50d97016574d57d3e11360c237e92c4f41c5d967
Yang Yang <yangyang861115@gmail.com>
2016-08-18 04:53:17 +0000

    update SECRET env variable

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(flash());
 
 //PASSPORT CONFIGURATION
 app.use(expressSession({
-    secret : "my best friend",
+    secret : process.env.SECRET,
     resave : false,
     saveUninitialized: false
 }));
